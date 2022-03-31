eafc31240a566555ed6873997fa144766a5defe7
Gustavo N <gustavostnt@gmail.com>
2022-03-30 22:59:38 -0300

    scene 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 console.log("script loaded");
+
+const e = document.querySelector(".scene-3 img");
+
+const timer = (i = 1) => {
+  console.log("t");
+  setTimeout(() => {
+    console.log("tt", i);
+    // e.style.backgroundImage = `url("assets/img/slide_${i}_09.png")`;
+    e.src = `assets/img/slide_${i}_09.png`;
+    i < 4 ? timer(i + 1) : timer();
+  }, 1000);
+};
+
+timer();
