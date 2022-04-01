c0bdf3576622648f4b9e39a3eb47718ac62d71ce
Gustavo N <gustavostnt@gmail.com>
2022-04-01 00:20:31 -0300

    slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,37 @@
 console.log("script loaded");
 
-const e = document.querySelector(".scene-3 img");
+const images = document.querySelectorAll(".slider img");
+const slider = document.querySelector(".slider");
+let animation;
+let counter = 0;
 
-const timer = (i = 1) => {
-  setTimeout(() => {
-    e.src = `assets/img/slide_${i}_09.png`;
-    i < 4 ? timer(i + 1) : timer();
-  }, 1500);
+const prevClick = () => {
+  count(-1);
 };
 
-timer();
+const nextClick = () => {
+  count(+1);
+};
+
+const mouseEnter = () => {
+  clearInterval(animation);
+};
+
+const mouseLeave = () => {
+  animation = setInterval(nextClick, 2000);
+};
+
+const count = (c) => {
+  setImage((counter += c));
+};
+
+const setImage = (c) => {
+  if (c >= images.length) counter = 0;
+  else if (c < 0) counter = images.length - 1;
+  slider.style.marginLeft = `${-30 * counter}vw`;
+  console.log(counter);
+};
+
+slider.style.width = `${30 * images.length}vw`;
+animation = setInterval(nextClick, 1000);
+setImage(counter);
