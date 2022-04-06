1027d6f89f8c848eb95ff002a9535a4f309162c9
Gustavo N <gustavostnt@gmail.com>
2022-04-06 01:49:31 -0300

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,10 +28,10 @@ const count = (c) => {
 const setImage = (c) => {
   if (c >= images.length) counter = 0;
   else if (c < 0) counter = images.length - 1;
-  slider.style.marginLeft = `${-30 * counter}vw`;
-  console.log(counter);
+  slider.style.marginLeft = `${-images[0]?.offsetWidth * counter}px`;
+  // console.log(counter);
 };
 
-slider.style.width = `${30 * images.length}vw`;
+slider.style.width = `${images[0]?.offsetWidth * images.length + 10}px`;
 animation = setInterval(nextClick, 1000);
 setImage(counter);
